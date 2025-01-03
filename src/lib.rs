b79c73f6fca287344912f34f394e422fc7cf69b7
David Martin <davmarti@redhat.com>
2025-01-03 15:21:38 +0000

    format
    Signed-off-by: David Martin <davmarti@redhat.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,8 @@
+use proxy_wasm::hostcalls::{define_metric, increment_metric};
 use proxy_wasm::traits::*;
 use proxy_wasm::types::*;
-use proxy_wasm::hostcalls::{define_metric, increment_metric};
 use serde::Deserialize;
 use serde_json::Value;
-
 
 #[derive(Deserialize, Debug)] // Derive Debug
 struct Usage {
@@ -63,48 +62,67 @@ impl Context for MyHttpFilter {}
 impl HttpContext for MyHttpFilter {
     fn on_http_request_headers(&mut self, _: usize, _: bool) -> Action {
         let _ = proxy_wasm::hostcalls::log(LogLevel::Info, "Wasm filter: Got request headers!");
-
         Action::Continue
     }
 
     fn on_http_response_headers(&mut self, _num_headers: usize, _end_of_stream: bool) -> Action {
         let _ = proxy_wasm::hostcalls::log(LogLevel::Info, "Wasm filter: Got response headers!");
-
         Action::Continue
     }
-    
+
     fn on_http_response_body(&mut self, body_size: usize, end_of_stream: bool) -> Action {
-        let _ = proxy_wasm::hostcalls::log(LogLevel::Info, "Wasm filter: on_http_response_body called!");
+        let _ = proxy_wasm::hostcalls::log(
+            LogLevel::Info,
+            "Wasm filter: on_http_response_body called!",
+        );
         if let Some(chunk) = self.get_http_response_body(0, body_size) {
-            let _ = proxy_wasm::hostcalls::log(LogLevel::Info, &format!("Wasm filter: Got response body chunk! size: {:?}", &chunk.len()));
+            let _ = proxy_wasm::hostcalls::log(
+                LogLevel::Info,
+                &format!(
+                    "Wasm filter: Got response body chunk! size: {:?}",
+                    &chunk.len()
+                ),
+            );
             self.response_body.extend_from_slice(&chunk);
         }
         if end_of_stream {
-            let _ = proxy_wasm::hostcalls::log(LogLevel::Info, &format!("Wasm filter: Got response body! body_size: {:?}", body_size));
-            // if let Some(body_bytes) = self.get_http_response_body(0, body_size) {
-            //     let _ = proxy_wasm::hostcalls::log(LogLevel::Info, &format!("Wasm filter: Got body bytes! size: {:?}", body_size));
-                if let Ok(body_str) = std::str::from_utf8(&self.response_body) {
-                    let _ = proxy_wasm::hostcalls::log(LogLevel::Info, &format!("Wasm filter: Got body string! str: {:?}", body_str));
-                    if let Ok(json_val) = serde_json::from_str::<Value>(body_str) {
-                        let _ = proxy_wasm::hostcalls::log(LogLevel::Info, &format!("Wasm filter: Got json value! json_val: {:?}", json_val));
-                        if let Some(usage_val) = json_val.get("usage") {
-                            let _ = proxy_wasm::hostcalls::log(LogLevel::Info, &format!("Wasm filter: Got usage value! usage_val: {:?}", usage_val));
-                            if let Ok(parsed) = serde_json::from_value::<Usage>(usage_val.clone()) {
-                                let _ = proxy_wasm::hostcalls::log(LogLevel::Info, &format!("Found usage: {:?}", parsed));
+            let _ = proxy_wasm::hostcalls::log(
+                LogLevel::Info,
+                &format!("Wasm filter: Got response body! body_size: {:?}", body_size),
+            );
+            if let Ok(body_str) = std::str::from_utf8(&self.response_body) {
+                let _ = proxy_wasm::hostcalls::log(
+                    LogLevel::Info,
+                    &format!("Wasm filter: Got body string! str: {:?}", body_str),
+                );
+                if let Ok(json_val) = serde_json::from_str::<Value>(body_str) {
+                    let _ = proxy_wasm::hostcalls::log(
+                        LogLevel::Info,
+                        &format!("Wasm filter: Got json value! json_val: {:?}", json_val),
+                    );
+                    if let Some(usage_val) = json_val.get("usage") {
+                        let _ = proxy_wasm::hostcalls::log(
+                            LogLevel::Info,
+                            &format!("Wasm filter: Got usage value! usage_val: {:?}", usage_val),
+                        );
+                        if let Ok(parsed) = serde_json::from_value::<Usage>(usage_val.clone()) {
+                            let _ = proxy_wasm::hostcalls::log(
+                                LogLevel::Info,
+                                &format!("Found usage: {:?}", parsed),
+                            );
 
-                                // Increments must be i64 (not u64)
-                                let _ = increment_metric(self.metric_prompt_tokens, parsed.prompt_tokens);
-                                let _ = increment_metric(
-                                    self.metric_completion_tokens,
-                                    parsed.completion_tokens,
-                                );
-                                let _ = increment_metric(self.metric_total_tokens, parsed.total_tokens);
-
-                            }
+                            // Increments must be i64 (not u64)
+                            let _ =
+                                increment_metric(self.metric_prompt_tokens, parsed.prompt_tokens);
+                            let _ = increment_metric(
+                                self.metric_completion_tokens,
+                                parsed.completion_tokens,
+                            );
+                            let _ = increment_metric(self.metric_total_tokens, parsed.total_tokens);
                         }
                     }
                 }
-            // }
+            }
         }
         Action::Continue
     }
